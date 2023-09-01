06de5d0a5e606e8c04d7204759851100b5384f54
Matteo Castellucci <matteo.castellucci@outlook.com>
2023-09-01 23:31:02 +0200

    fix: add service paths for web UI

--- master/src/main/scala/laas/master/ws/service/ServiceController.scala
+++ master/src/main/scala/laas/master/ws/service/ServiceController.scala
@@ -25,33 +25,28 @@ package laas.master.ws.service
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.UUID
-
 import scala.concurrent.ExecutionContextExecutor
 import scala.concurrent.Future
 import scala.concurrent.duration.DurationInt
-
 import akka.NotUsed
 import akka.actor.ActorSystem as ClassicActorSystem
 import akka.actor.typed.ActorRef
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.DispatcherSelector
 import akka.actor.typed.scaladsl.AskPattern.*
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.{ContentType, HttpCharsets, MediaTypes, StatusCodes}
 import akka.http.scaladsl.model.ws.BinaryMessage
 import akka.http.scaladsl.model.ws.Message
 import akka.http.scaladsl.model.ws.TextMessage
 import akka.http.scaladsl.server.Directives.*
 import akka.http.scaladsl.server.Route
 import akka.stream.OverflowStrategy
-import akka.stream.scaladsl.Flow
-import akka.stream.scaladsl.Sink
-import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.{Flow, Sink, Source}
 import akka.stream.typed.scaladsl.ActorSink
 import akka.stream.typed.scaladsl.ActorSource
 import akka.util.Timeout
 import io.circe.parser.*
 import io.circe.syntax.EncoderOps
-
 import laas.master.model.Execution.{ExecutionArguments, ExecutionOutput}
 import laas.master.model.Executable.ExecutableType
 import laas.master.ws.presentation.{Request, Response}
@@ -65,62 +60,71 @@ object ServiceController {
   def apply(api: ActorRef[ServiceApiCommand])(using actorSystem: ActorSystem[Nothing]): Route = {
     given ClassicActorSystem = actorSystem.classicSystem
     given ExecutionContextExecutor = actorSystem.dispatchers.lookup(DispatcherSelector.blocking())
-
     concat(
-      path("service") {
-        handleWebSocketMessages {
-          val (actorRef: ActorRef[Response], source: Source[Response, NotUsed]) =
-            ActorSource
-              .actorRef[Response](PartialFunction.empty, PartialFunction.empty, 1, OverflowStrategy.dropHead)
-              .preMaterialize()
-          val id = UUID.randomUUID()
-          api ! ServiceApiCommand.Open(actorRef, id)
-          Flow[Message]
-            .mapAsync(1) {
-              case m: TextMessage.Strict => Future.successful(m)
-              case m: TextMessage.Streamed => m.textStream.runFold("")(_ + _).map(TextMessage.apply)
-              case m: BinaryMessage => m.dataStream.runWith(Sink.ignore).map(_ => null)
+      pathPrefix("service") {
+        Route.seal(
+          concat(
+            path("ws") {
+              handleWebSocketMessages {
+                val (actorRef: ActorRef[Response], source: Source[Response, NotUsed]) =
+                  ActorSource
+                    .actorRef[Response](PartialFunction.empty, PartialFunction.empty, 1, OverflowStrategy.dropHead)
+                    .preMaterialize()
+                val id = UUID.randomUUID()
+                api ! ServiceApiCommand.Open(actorRef, id)
+                Flow[Message]
+                  .mapAsync(1) {
+                    case m: TextMessage.Strict => Future.successful(m)
+                    case m: TextMessage.Streamed => m.textStream.runFold("")(_ + _).map(TextMessage.apply)
+                    case m: BinaryMessage => m.dataStream.runWith(Sink.ignore).map(_ => null)
+                  }
+                  .flatMapConcat(t =>
+                    (for {
+                      j <- parse(t.text).toOption
+                      m <- j.as[Request].toOption
+                    } yield ServiceApiCommand.RequestCommand(m, actorRef))
+                      .map(Source.single[ServiceApiCommand])
+                      .getOrElse(Source.empty[ServiceApiCommand])
+                  )
+                  .via(
+                    Flow.fromSinkAndSourceCoupled(
+                      ActorSink.actorRef[ServiceApiCommand](
+                        api,
+                        ServiceApiCommand.Close(actorRef, id),
+                        _ => ServiceApiCommand.Close(actorRef, id)
+                      ),
+                      source
+                    )
+                  )
+                  .map(r => TextMessage(r.asJson.noSpaces))
+              }
+            },
+            path("deploy") {
+              formFields("name", "id".as[UUID]) { (name, id) =>
+                val executableId = UUID.randomUUID()
+                storeUploadedFile(
+                  "file",
+                  i => Files.createFile(Paths.get(executableId.toString)).toFile
+                ) { (fileInfo, file) =>
+                  api ! ServiceApiCommand.Deploy(
+                    executableId,
+                    ExecutableType.getByExtension(fileInfo.fileName.split('.')(1)),
+                    name,
+                    id
+                  )
+                  complete(StatusCodes.OK)
+                }
+              }
+            },
+            path("files" / JavaUUID) { id =>
+              getFromFile(id.toString)
             }
-            .flatMapConcat(t =>
-              (for {
-                j <- parse(t.text).toOption
-                m <- j.as[Request].toOption
-              } yield ServiceApiCommand.RequestCommand(m, actorRef))
-                .map(Source.single[ServiceApiCommand])
-                .getOrElse(Source.empty[ServiceApiCommand])
-            )
-            .via(
-              Flow.fromSinkAndSourceCoupled(
-                ActorSink.actorRef[ServiceApiCommand](
-                  api,
-                  ServiceApiCommand.Close(actorRef, id),
-                  _ => ServiceApiCommand.Close(actorRef, id)
-                ),
-                source
-              )
-            )
-            .map(r => TextMessage(r.asJson.noSpaces))
-        }
+          )
+        )
       },
-      path("deploy") {
-        formFields("name", "id".as[UUID]) { (name, id) =>
-          val executableId = UUID.randomUUID()
-          storeUploadedFile(
-            "file",
-            i => Files.createFile(Paths.get(executableId.toString)).toFile
-          ) { (fileInfo, file) =>
-            api ! ServiceApiCommand.Deploy(
-              executableId,
-              ExecutableType.getByExtension(fileInfo.fileName.split('.')(1)),
-              name,
-              id
-            )
-            complete(StatusCodes.OK)
-          }
-        }
-      },
-      path("files" / JavaUUID) { id =>
-        getFromFile(id.toString)
+      getFromResourceDirectory("ui"),
+      pathPrefix("") {
+        getFromResource("ui/index.html", ContentType(MediaTypes.`text/html`, HttpCharsets.`UTF-8`))
       }
     )
   }
